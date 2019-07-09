aa4ea7a11b49c07c5ff9e41965ff43a01828849e
timo.heiten <heitech.heiten@gmail.com>
2019-07-09 23:32:16 +0200

    added Consumer RabbitMQ.Client

--- csharp/consumer/Program.cs
+++ csharp/consumer/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace consumer
 {
     class Program
     {
-        static void Main(string[] args)
+          static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string key = string.Empty;
+            try
+            {   
+                key = args[1];
+                s_runner[key].Invoke();
+            }
+            catch (System.Exception ex)
+            {
+                System.Console.WriteLine(ex.Message);
+            }
+            System.Console.WriteLine("-".PadRight(50));
+            System.Console.WriteLine($"ran with key: {key}");
+            Thread.Sleep(500);
         }
+
+         static Program()
+        {
+            s_runner = new Dictionary<string, Action>
+            {
+                ["consume"] = consumer.mq_client.Consumer.Run
+            };
+        }
+
+        private static Dictionary<string, Action> s_runner;
     }
 }
--- csharp/consumer/mq_client/Consumer.cs
+++ csharp/consumer/mq_client/Consumer.cs
@@ -2,11 +2,47 @@ using System;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Collections.Generic;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
 namespace consumer.mq_client
 {
     internal class Consumer
     {
-        
+        public static void Run()
+        {
+            // connection
+            IConnection connection = Create().CreateConnection();
+            // channel
+            using (var channel = connection.CreateModel()) // IModel
+            {
+                // basicConsume
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (ch, eventArgs) => 
+                {
+                    var body = eventArgs.Body;
+                    System.Console.WriteLine("message Received:");
+                    System.Console.WriteLine(Encoding.UTF8.GetString(body));
+                    channel.BasicAck(eventArgs.DeliveryTag, multiple:false);
+                };
+                string tag = channel.BasicConsume("pika_queue", autoAck:false, consumer);
+                Console.WriteLine("connection established! Consuming in the background!");
+                Console.ReadKey();
+            }
+        }
+
+        private static ConnectionFactory Create()
+        {
+            return new ConnectionFactory
+            {
+                HostName = "localhost",
+                Port = 5672,
+                UserName = "guest",
+                Password = "guest",
+                VirtualHost = "/",
+                Protocol = Protocols.DefaultProtocol
+            };
+        }
     }
 }
