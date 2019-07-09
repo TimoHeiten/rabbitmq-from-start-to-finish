2eb0fbd1de542c1db1649aa08f22b95afc7115bd
timo.heiten <heitech.heiten@gmail.com>
2019-07-09 21:36:04 +0200

    scaffold

---
+++ csharp/consumer/mq_client/Consumer.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace consumer.mq_client
+{
+    internal class Consumer
+    {
+        
+    }
+}
---
+++ csharp/messages/Connection/Stats.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace messages.Connection
+{
+    public static class Stats
+    {
+ 
+    }
+}
